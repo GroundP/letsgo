f222eec62c2bfc823154bff12a4eee0c2775ac09
GroundP <bloom0819@naver.com>
2021-12-26 15:10:50 +0900

    Practice Inheritance

---
+++ src/InheritancePrac.java
@@ -0,0 +1,42 @@
+class Parent{
+    String name;
+    void setName(String _name){
+        name = _name;
+    }
+
+    void printName(){
+        System.out.println(name);
+    }
+}
+
+public class InheritancePrac extends Parent {
+    InheritancePrac(){
+        System.out.println("Constructor without param");
+    }
+
+    InheritancePrac(int i){
+        System.out.println("Constructor without param: " + i);
+    }
+    void sleep()
+    {
+        System.out.println("zzz");
+    }
+
+    void printName(){
+        System.out.println("Child name is "+name);
+    }
+
+}
+
+class Main{
+    public static void main(String[] args) {
+        InheritancePrac child = new InheritancePrac();
+        child.setName("SEOA");
+        child.printName();
+        child.sleep();
+
+        Parent parentClass = new InheritancePrac(6);
+        parentClass.setName("YJ");
+        parentClass.printName();
+    }
+}
